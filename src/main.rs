0a453dc7a1bd929c6e8ca7965d4b8f06ba0a88a9
Magnus Manske <mm6@sanger.ac.uk>
2022-05-04 11:12:39 +0100

    --check option

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use flate2::read::MultiGzDecoder;
 use std::fs::File;
 use std::io::{stdin, stdout, BufRead, BufReader, BufWriter};
 use rayon::prelude::*;
+use clap::Parser;
 
 pub struct FileReader {
     _filename: String,
@@ -33,12 +34,27 @@ impl FileReader {
     pub fn check_meta(&self, origin: &Self) -> bool {
         (origin.vcf_record.chromosome == self.vcf_record.chromosome) &&
         (origin.vcf_record.position == self.vcf_record.position) &&
+        (origin.vcf_record.id == self.vcf_record.id) &&
         (origin.vcf_record.reference == self.vcf_record.reference) &&
         (origin.vcf_record.alternative == self.vcf_record.alternative)
     }
 }
 
+#[derive(Parser, Debug)]
+#[clap(author="Magnus Manske <magnusmanske@googlemail.com>", version="0.0.2", about, long_about = None)]
+/// Merge large numbers of VCF files with identical (CROM/POS/ID/REF/ALT) variants.
+/// Pipe in a list of vcf.gz files; umcompressed VCF will be written to STDOUT.
+struct Args {
+    /// Check for identical CHROM, POS, ID, REF, ALT in every file; ~5% slower
+    #[clap(short, long)]
+    check: bool,
+
+}
+
 fn main() {
+    let args = Args::parse();
+    println!("{:?}",&args);
+
     let mut readers : Vec<FileReader> = stdin().lock().lines().map(|line|{FileReader::new(&line.unwrap())}).collect();
     let all_samples : Vec<U8Vec> = readers.iter().map(|reader|reader.reader.header().samples().to_owned()).flatten().collect();
     let mut items : Vec<VCFHeaderLine> = readers[0].reader.header().items().to_vec() ;
@@ -61,19 +77,19 @@ fn main() {
         })
         .sum();
         if total_read != readers.len() {
-            if total_read > 0 {
-                println!("Could only read from {} of {} files on data row {}",total_read,readers.len(),row);
+            if total_read > 0 { // Some VCF files were read but some were not, that's a problem
+                panic!("Could only read from {} of {} files on data row {}",total_read,readers.len(),row);
             }
-            break
-        }
-        
-        /*
-        // Paranoia
-        if readers.iter().any(|x|{!x.check_meta(&readers[0])}) {
-            println!("Row {} has a problem!",row);
+            // All VCF files have reached the end simultaneously, we're done!
             break ;
         }
-        */
+        
+        if args.check {
+            if readers.iter().any(|x|{!x.check_meta(&readers[0])}) {
+                panic!("Row {} has a problem with CROM/POS/ID/REF/ALT.",row);
+            }
+        }
+
         let mut joined_vcf_record = readers[0].vcf_record.clone();
         readers.iter_mut().skip(1).for_each(|record|{
             joined_vcf_record.genotype.append(&mut record.vcf_record.genotype);
